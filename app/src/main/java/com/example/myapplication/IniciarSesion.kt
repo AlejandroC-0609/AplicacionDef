0d3bb89595c9646b8629bc270788dca228a94c28
AlejandroC-0609 <johan.carrillof@utadeo.edu.co>
2024-10-14 15:53:00 -0500

    Actualizacion

---
+++ app/src/main/java/com/example/myapplication/IniciarSesion.kt
@@ -0,0 +1,32 @@
+package com.example.myapplication
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class IniciarSesion : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_iniciar_sesion)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val iniciarsesion1: Button = findViewById(R.id.iniciarsesion1)
+        iniciarsesion1.setOnClickListener {
+            Intent(this, Recetas::class.java)
+                .also { welcomeIntent ->
+                    //Launch
+                    startActivity(welcomeIntent)
+                }
+    }
+}
+    }
