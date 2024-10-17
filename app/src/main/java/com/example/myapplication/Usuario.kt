baf8279ffa6225669a7c7a123cb5876a13c0313b
AlejandroC-0609 <johan.carrillof@utadeo.edu.co>
2024-10-17 16:41:21 -0500

    Actualizacion

---
+++ app/src/main/java/com/example/myapplication/Usuario.kt
@@ -0,0 +1,30 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Usuario : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_usuario)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val Button5: Button = findViewById(R.id.Button5)
+        Button5.setOnClickListener {
+            Intent(this, Recetas::class.java)
+                .also { welcomeIntent ->
+                    //Launch
+                    startActivity(welcomeIntent)
+                }
+        }
+    }
+}
